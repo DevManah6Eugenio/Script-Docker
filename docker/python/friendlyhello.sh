c79e5b307d45f9ee4be64743b0da02d613094a00
Manasses Java <manasses.java@gmail.com>
2018-12-14 10:52:31 -0300

    rodar o aplicativo em segundo plano
    rodar o aplicativo em segundo plano, no modo desanexado

--- docker/python/friendlyhello.sh
+++ docker/python/friendlyhello.sh
@@ -1,2 +1,5 @@
 #executa o container friendlyhello
-sudo docker run -p 4000:80 friendlyhello
+#sudo docker run -p 4000:80 friendlyhello
+
+#rodar o aplicativo em segundo plano, no modo desanexado
+sudo docker run -d -p 4000:80 friendlyhello
